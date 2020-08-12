8745eb1abb9823523a0dc4df29919caf4245e5f9
twenty-one-savage <alohafrombali@gmail.com>
2020-08-12 10:34:50 +0300

    4.1.1 Рефакторинг рендер-метода

--- src/main.js
+++ src/main.js
@@ -12,47 +12,44 @@ import {createAppMostCommentedFilmsTemplate} from './view/most-commented-films.j
 import {createAppFilmStatisticsTemplate} from './view/film-statistics.js';
 import {generateFilms} from './mock/film.js';
 import {generateFilter} from './mock/filter.js';
+import {renderTemplate} from './utils.js';
 
 
 const films = generateFilms();
 
 const filters = generateFilter(films);
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
-
 const appHeaderElement = document.querySelector(`.header`);
 
-render(appHeaderElement, createAppUserTemplate(), `beforeend`);
+renderTemplate(appHeaderElement, createAppUserTemplate(), `beforeend`);
 
 const appMainElement = document.querySelector(`.main`);
 
-render(appMainElement, createAppSortTemplate(), `afterbegin`);
+renderTemplate(appMainElement, createAppSortTemplate(), `afterbegin`);
 
-render(appMainElement, createAppMainNavigationTemplate(filters), `afterbegin`);
+renderTemplate(appMainElement, createAppMainNavigationTemplate(filters), `afterbegin`);
 
-render(appMainElement, createAppFilmsListTemplate(), `beforeend`);
+renderTemplate(appMainElement, createAppFilmsListTemplate(), `beforeend`);
 
 const appFilmsElement = appMainElement.querySelector(`.films`);
 const appFilmsListElement = appFilmsElement.querySelector(`.films-list`);
 const appFilmsListContainerElement = appFilmsListElement.querySelector(`.films-list__container`);
 
 for (let i = 0; i < Math.min(films.length, FilmsCount.PER_STEP); i++) {
-  render(appFilmsListContainerElement, createAppFilmTemplate(films[i]), `beforeend`);
+  renderTemplate(appFilmsListContainerElement, createAppFilmTemplate(films[i]), `beforeend`);
 }
 
 if (films.length > FilmsCount.PER_STEP) {
   let renderedFilmsCount = FilmsCount.PER_STEP;
 
-  render(appFilmsListElement, createAppBtnShowMoreTemplate(), `beforeend`);
+  renderTemplate(appFilmsListElement, createAppBtnShowMoreTemplate(), `beforeend`);
 
   const loadMoreButton = appFilmsListElement.querySelector(`.films-list__show-more`);
 
   loadMoreButton.addEventListener(`click`, (evt) => {
     evt.preventDefault();
     films.slice(renderedFilmsCount, renderedFilmsCount + FilmsCount.PER_STEP)
-    .forEach((film) => render(appFilmsListContainerElement, createAppFilmTemplate(film), `beforeend`));
+    .forEach((film) => renderTemplate(appFilmsListContainerElement, createAppFilmTemplate(film), `beforeend`));
 
     renderedFilmsCount += FilmsCount.PER_STEP;
 
@@ -65,20 +62,20 @@ if (films.length > FilmsCount.PER_STEP) {
 
 const appBodyElement = document.querySelector(`body`);
 
-render(appBodyElement, createAppFilmPopupTemplate(films[getRandomInteger(0, films.length - 1)]), `beforeend`);
+renderTemplate(appBodyElement, createAppFilmPopupTemplate(films[getRandomInteger(0, films.length - 1)]), `beforeend`);
 
-render(appFilmsElement, createAppTopRatedFilmsTemplate(), `beforeend`);
-render(appFilmsElement, createAppMostCommentedFilmsTemplate(), `beforeend`);
+renderTemplate(appFilmsElement, createAppTopRatedFilmsTemplate(), `beforeend`);
+renderTemplate(appFilmsElement, createAppMostCommentedFilmsTemplate(), `beforeend`);
 
 const appFilmsExtraElements = appFilmsElement.querySelectorAll(`.films-list--extra`);
 
 for (let i = 0; i < appFilmsExtraElements.length; i++) {
   const el = appFilmsExtraElements[i].querySelector(`.films-list__container`);
   for (let j = 0; j < FilmsCount.EXTRA; j++) {
-    render(el, createAppFilmTemplate(films[i]), `beforeend`);
+    renderTemplate(el, createAppFilmTemplate(films[i]), `beforeend`);
   }
 }
 
 const appFooterElement = appBodyElement.querySelector(`.footer`);
 
-render(appFooterElement, createAppFilmStatisticsTemplate(), `beforeend`);
+renderTemplate(appFooterElement, createAppFilmStatisticsTemplate(), `beforeend`);
--- src/utils.js
+++ src/utils.js
@@ -6,3 +6,15 @@ export const getRandomInteger = (a = 0, b = 1) => {
 
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
+
+export const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
